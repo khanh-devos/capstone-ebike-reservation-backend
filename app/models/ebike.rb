1dc564614abb204e62eb6cbba6a7084de38a9f86
khanh-devos <dreamproperty.khanh@gmail.com>
2023-11-03 20:16:41 +0700

    fix reservation relation

--- app/models/ebike.rb
+++ app/models/ebike.rb
@@ -2,6 +2,7 @@ class Ebike < ApplicationRecord
   has_one_attached :image
 
   has_many :reservations, foreign_key: 'user_id'
+
   belongs_to :user, foreign_key: 'seller_id'
 
   validates :name, presence: true
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,4 @@
 class Reservation < ApplicationRecord
-  belongs_to :ebike, foreign_key: 'user_id'
+  belongs_to :ebike, foreign_key: 'ebike_id'
+  belongs_to :user, foreign_key: 'user_id'
 end
