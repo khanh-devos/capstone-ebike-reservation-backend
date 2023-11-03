1100583bf65c83c25aef9e1b41a2b4ce94fbd961
khanh-devos <dreamproperty.khanh@gmail.com>
2023-11-03 14:14:26 +0700

    Remove reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,14 +1,3 @@
 class Reservation < ApplicationRecord
   belongs_to :ebike, foreign_key: 'user_id'
-
-  validates :it_is_date?
-  validates :it_is_date?
-
-  private
-
-  def it_is_date?
-    return null unless !starting_date.is_a?(Date) || !ending_date.is_a?(Date)
-
-    errors.add(:starting_date, 'must be a valid date')
-  end
 end
