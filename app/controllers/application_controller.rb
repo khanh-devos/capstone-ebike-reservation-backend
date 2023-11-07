5c2a7581208877afa0c0e23ee2fce99ce82ebba0
khanh-devos <dreamproperty.khanh@gmail.com>
2023-11-07 14:22:38 +0700

    Disable authentication check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include JwtApi
-  before_action :authenticate_request, except: %i[login register]
+  # before_action :authenticate_request, except: %i[login register]
 
   private
 
