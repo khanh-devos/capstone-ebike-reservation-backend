ed0a09a76bdad9fbdec4c8b046618c6ef4dce3dc
souad988 <souadelmansouri88@gmail.com>
2023-11-07 11:09:17 +0100

    Add image to ebikes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_153618) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_100741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_153618) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["seller_id"], name: "index_ebikes_on_seller_id"
   end
 
